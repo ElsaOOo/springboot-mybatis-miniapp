d0efcc2a571a45e1a1efbd2e27e488675084938c
YeJingjing <yjjjng0901@163.com>
2019-04-08 21:33:28 +0800

    init project

---
+++ src/main/java/com/yezi/demoa/DemoaApplication.java
@@ -0,0 +1,13 @@
+package com.yezi.demoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoaApplication.class, args);
+    }
+
+}
