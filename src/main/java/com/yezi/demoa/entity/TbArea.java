e58b73f96a6e2438d04aa3d9ddc57fa201bfe9fa
YeJingjing <yjjjng0901@163.com>
2019-04-09 09:07:34 +0800

    update database

---
+++ src/main/java/com/yezi/demoa/entity/TbArea.java
@@ -0,0 +1,41 @@
+package com.yezi.demoa.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @Author: yezi
+ * @Date: 2019/4/9 08 12
+ * @Description:
+ */
+@Entity
+@Table(name = "tb_area")
+@DynamicInsert
+@DynamicUpdate
+@Data
+public class TbArea {
+
+    // 主键ID 类型用基础类型的包装类型，方便做非空的校验
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Integer area_id;
+
+    // 名称
+    @Column(name = "area_name")
+    private String areaName;
+
+    // 权重，越大越排名靠前
+    private Integer priority;
+
+    // 创建时间
+    @Column(name = "create_time")
+    private Date createTime;
+
+    // 最近一次修改时间
+    @Column(name = "last_edit_time")
+    private Date lastEdiTime;
+}
